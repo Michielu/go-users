98ca840cd211b12b1831e42123c6512f1b8dcecc
Michielu Menning <mich.menning@gmail.com>
2020-06-30 20:40:26 -0500

    Initial go files

---
+++ hello.go
@@ -0,0 +1,9 @@
+//The first statement in a Go source file must be package name. 
+//Executable commands must always use package main.package main
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+}
