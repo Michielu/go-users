1f6f363946ac62e85a421781028aa3f91478414b
Michielu Menning <mich.menning@gmail.com>
2020-07-03 20:34:26 -0500

    Connect to aws server

--- app/server/server.go
+++ app/server/server.go
@@ -2,8 +2,13 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 /**TODO:
@@ -29,15 +34,57 @@ type Server struct {
 	Handler   http.Handler
 }
 
+type Item struct {
+	userId    string
+	createdAt int
+	username  string
+}
+
 func (s *Server) Start() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	tableName := "MyUsers"
+	getUser := "1a2b3c4d"
+
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"userId": {
+				S: aws.String(getUser),
+			},
+			// "userId": aws.String(getUser),
+		},
+	})
+
+	log.Println("b", result)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	creds, err := sess.Config.Credentials.Get()
+	if err != nil {
+		log.Fatal("a", err) //TODO: panic and recover
+	}
+	log.Println("a", creds)
 	http.HandleFunc("/", HelloServer)
+	// log.Fatal(creds)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	// fmt.Println(w, "Hi %d", time.Now())
 	//Add some logger
 	t := Thing{42, r.URL.Path[1:]}
+	log.Println(r.URL.Path[1:])
 	j, err := json.Marshal(t)
 	if err != nil {
 		panic(err)
@@ -45,6 +92,4 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	jsonData := []byte(j)
 	w.Write(jsonData)
-
-	// fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
